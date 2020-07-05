05007fa0e9a69b2ae2e8c2c945087d6d70eaee19
YangHan-Morningstar <imuyanghan@gmail.com>
2020-07-05 22:07:29 +0800

    Initial Commit

---
+++ News Prosecutor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News Prosecutor
+//
+//  Created by Tony Young on 2020/7/5.
+//  Copyright © 2020 YangHan-Morningstar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
