e2ff148d7a79072ff48d3fe2c90ecc1fb3b6ee0c
YangHan-Morningstar <imuyanghan@gmail.com>
2020-07-10 11:05:02 +0800

    添加有字符限制的TextField，用于用户登录

---
+++ News Prosecutor/Services/CharacterLimitTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterLimitTextField.swift
+//  News Prosecutor
+//
+//  Created by Tony Young on 2020/7/10.
+//  Copyright © 2020 YangHan-Morningstar. All rights reserved.
+//
+
+import SwiftUI
+
+struct CharacterLimitTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterLimitTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterLimitTextField()
+    }
+}
